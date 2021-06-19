23f5075530f29e654e7aded57e01e5979d8afbca
KoDaI YaNo <kodai.developer@gmail.com>
2021-06-19 13:16:18 +0900

    整理しました

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+// markup
+const hello = "意味：こんにちは"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>コニチハ {hello}</h1>
+    </main>
+  )
+}
+
+export default IndexPage
